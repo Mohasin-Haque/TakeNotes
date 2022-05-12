f89d16a3eeb8c2eed557aa0156b240d912d785c6
Mohasin Haque <mohasinhaque23121@gmail.com>
2022-05-12 19:40:06 +0530

    Refactored: Implemented barrel exports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.css";
-import { LandingPage } from "./pages/LandingPage/LandingPage";
 import { Routes, Route } from "react-router-dom";
-import { HomePage } from "./pages/HomePage/HomePage";
-import { ArchivePage } from "./pages/ArchivePage/ArchivePage";
-import { TrashPage } from "./pages/TrashPage/TrashPage";
-import { ProfilePage } from "./pages/ProfilePage/ProfilePage";
+import { HomePage, ArchivePage, LandingPage, TrashPage, ProfilePage } from "./pages";
+
 
 
 function App() {
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import { LandingPage } from "./LandingPage/LandingPage";
+import { HomePage } from "./HomePage/HomePage";
+import { ProfilePage } from "./ProfilePage/ProfilePage";
+import { TrashPage } from "./TrashPage/TrashPage";
+import { ArchivePage } from "./ArchivePage/ArchivePage";
+
+
+export { HomePage, LandingPage, TrashPage, ProfilePage, ArchivePage }
