081068e3a89159c674a7dcb7b40be151808e82c5
Mohasin Haque <mohasinhaque23121@gmail.com>
2022-05-22 13:10:45 +0530

    refactor - break the code into components

---
+++ src/components/AuthComp/SignupPageComp.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+const SignupPageComp = () => {
+    return(
+        <div className="signup-container">
+                <form className="flex">
+                    <div className="input-container font-size-one">
+                        <p>Full Name</p>
+                        <input className="input" type="text" placeholder="Enter your full name" />
+                    </div>
+                    <div className="input-container font-size-one">
+                        <p>Email address</p>
+                        <input className="input" type="text" placeholder="take@notes.com" />
+                    </div>
+                    <div className="input-container font-size-one">
+                        <p>Password</p>
+                        <input className="input" type="text" placeholder="********" />
+                    </div>
+                    <div className="input-container font-size-one">
+                        <p>Confirm Password</p>
+                        <input className="input" type="text" placeholder="Re-Enter your password" />
+                    </div>
+                    <div className="radio-actions">
+                        <label for="radio-unchecked">
+                            <input className="radio-unchecked gap" name="radio" type="checkbox" />
+                            I accept Terms & Conditions
+                        </label>
+                    </div>
+                    <button className="button cursor margin-zero">Signup</button>
+                    <div className="flex row">
+                    <p className="signup-text font-size-one">Already have an account</p>
+                    <Link to="/login" className="padding-one active-text cursor">Login</Link>
+                    </div>
+                </form>
+            </div>
+    )
+}
+
+export { SignupPageComp }
--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,34 +1,12 @@
-import { Navbar } from "../../components"
-import { Link } from "react-router-dom"
+import { LoginPageComp, Navbar } from "../../components"
+
 
 const LoginPage = () => {
     return(
         <div>
             <Navbar />
             <h1 className="flex">Login</h1>
-            <div className="signup-container">
-                <form className="flex">
-                    <div className="input-container font-size-one">
-                        <p>Email address</p>
-                        <input className="input" type="text" placeholder="take@notes.com" />
-                    </div>
-                    <div className="input-container font-size-one">
-                        <p>Password</p>
-                        <input className="input" type="text" placeholder="********" />
-                    </div>
-                    <div className="radio-actions">
-                        <label for="radio-unchecked">
-                            <input className="radio-unchecked gap" name="radio" type="checkbox" />
-                            Remember Me
-                        </label>
-                    </div>
-                    <button className="button cursor margin-zero">Login</button>
-                    <div className="flex row">
-                    <p className="signup-text font-size-one">New user?</p>
-                    <Link to="/login" className="padding-one active-text cursor">Signup</Link>
-                    </div>
-                </form>
-            </div>
+            <LoginPageComp />
         </div>
     )
 }
