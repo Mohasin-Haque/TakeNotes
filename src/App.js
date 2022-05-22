45c017b754089dcef82ebf4747f3a53ccfb7ca5e
Mohasin Haque <mohasinhaque23121@gmail.com>
2022-05-22 12:58:50 +0530

    Feat- added auth pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { HomePage, ArchivePage, LandingPage, TrashPage, ProfilePage } from "./pages";
+import { HomePage, ArchivePage, LandingPage, TrashPage, ProfilePage, SignupPage, LoginPage } from "./pages";
 
 
 
@@ -12,6 +12,8 @@ function App() {
       <Route path="/archive" element={<ArchivePage />} />
       <Route path="/trash" element={<TrashPage />} />
       <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
     </Routes>
   );
 }
--- src/components/LandingPageComp/LandingPageComp.jsx
+++ src/components/LandingPageComp/LandingPageComp.jsx
@@ -13,7 +13,8 @@ const LandingPageComp = () =>{
                        <p className="description"> Manage your daily tasks and workflow in a modern way and boost your efficiency without any efforts</p>
                    </div>
                       <Link to="/home"><button className="button cursor">Join Now</button></Link> 
-                       <Link to="/login"><p className="description active-text cursor">Already have an account?</p></Link>  
+                       <Link to="/login"><p className="description active-text cursor">Already have an account?</p></Link> 
+                       <Link to="/signup"><p className="description active-text cursor">Signup</p></Link> 
                </div>
                <img className="image" src={notesLogo} alt="error" />
                </div>
